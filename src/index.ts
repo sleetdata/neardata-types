c35aa036137046e2a56959033ba7eab9e5dec10f
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-01 07:19:00 -0500

    re z

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-// Index file that re-exports all NEAR data types
-// This provides a single entry point for importing types
+// Index file that re-exports all NEAR data types and Zod schemas
+// This provides a single entry point for importing types and schemas
 // ==============================================
-// Re-export all block-related types
-// master 🟨 neardata_block_response_interface
-export type * from "./interface/block_response.ts";
-// other
-export type * from "./interface/block_response_block_chunks.ts";
-export type * from "./interface/block_response_shards.ts";
-export type * from "./interface/block_response_shards.ts";
-export type * from "./interface/receipts.ts";
-// transactions
-export type * from "./interface/transactions.ts";
-export type * from "./interface/transaction_actions_AddKey.ts";
-export type * from "./interface/transactions_actions_CreateAccount.ts";
-export type * from "./interface/transactions_actions_Delegate.ts";
-export type * from "./interface/transactions_actions_DeleteAccount.ts";
-export type * from "./interface/transactions_actions_FunctionCall.ts";
-export type * from "./interface/transactions_actions_Transfer.ts";
+export type * from "./interface/index.ts";
+export * from "./zod/index.ts";
 // ==============================================
 // copyright 2025 by sleet.near
---
+++ src/zod/index.ts
@@ -0,0 +1,20 @@
+// Index file that re-exports all NEAR Zod schemas
+// This provides a single entry point for importing Zod schemas
+// ==============================================
+// Re-export all block-related Zod schemas
+// master 🟨 neardata_block_response_interface
+export * from "./block_response.ts";
+// other
+export * from "./block_response_block_chunks.ts";
+export * from "./block_response_shards.ts";
+export * from "./receipts.ts";
+// transactions
+export * from "./transactions.ts";
+export * from "./transaction_actions_AddKey.ts";
+export * from "./transactions_actions_CreateAccount.ts";
+export * from "./transactions_actions_Delegate.ts";
+export * from "./transactions_actions_DeleteAccount.ts";
+export * from "./transactions_actions_FunctionCall.ts";
+export * from "./transactions_actions_Transfer.ts";
+// ==============================================
+// copyright 2025 by sleet.near
